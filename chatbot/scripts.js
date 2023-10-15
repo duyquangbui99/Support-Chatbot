59bd4fb2cfdc2b9a118f126c9a52d6873855b49c
duyquangbui99 <77463586+duyquangbui99@users.noreply.github.com>
2023-10-15 16:46:28 -0500

    Update scripts.js

--- chatbot/scripts.js
+++ chatbot/scripts.js
@@ -1,76 +1,84 @@
-    document.addEventListener("DOMContentLoaded", function () {
-        const messageInput = document.getElementById("message-input");
-        const chatboxContent = document.getElementById("chatbox-content");
-        const sendButton = document.querySelector(".send-btn");
+document.addEventListener("DOMContentLoaded", function () {
+    const messageInput = document.getElementById("message-input");
+    const chatboxContent = document.getElementById("chatbox-content");
+    const sendButton = document.querySelector(".send-btn");
+    const chatboxContainer = document.querySelector(".chatbox-content");
 
-        // Load the intents from the JSON file (replace with your actual JSON URL)
-        fetch('intents.json')
-            .then(response => response.json())
-            .then(data => {
-                const intents = data.intents;
 
-                function botReply(userMessage) {
-                    const userMessageContainer = document.createElement("div");
-                    userMessageContainer.classList.add("user-message-container");
+    // Load the intents from the JSON file (replace with your actual JSON URL)
+    fetch('intents.json')
+        .then(response => response.json())
+        .then(data => {
+            const intents = data.intents;
 
-                    const userMessageContent = document.createElement("div");
-                    userMessageContent.classList.add("user-message-content");
+            function botReply(userMessage) {
+                const userMessageContainer = document.createElement("div");
+                userMessageContainer.classList.add("user-message-container");
 
-                    const userMessageElement = document.createElement("p");
-                    userMessageElement.classList.add("user-message");
-                    userMessageElement.textContent = userMessage;
+                const userMessageContent = document.createElement("div");
+                userMessageContent.classList.add("user-message-content");
 
-                    userMessageContent.appendChild(userMessageElement);
-                    userMessageContainer.appendChild(userMessageContent);
-                    chatboxContent.appendChild(userMessageContainer);
+                const userMessageElement = document.createElement("p");
+                userMessageElement.classList.add("user-message");
+                userMessageElement.textContent = userMessage;
 
-                    // Find the best response based on patterns
-                    let botResponse = "I'm sorry, I don't understand your message.";
-                    for (const intent of intents) {
-                        for (const pattern of intent.patterns) {
-                            const regex = new RegExp(pattern, "i"); // Case-insensitive match
-                            if (userMessage.match(regex)) {
-                                botResponse = intent.responses[Math.floor(Math.random() * intent.responses.length)];
-                                break; // Stop searching for patterns once a match is found
-                            }
+                userMessageContent.appendChild(userMessageElement);
+                userMessageContainer.appendChild(userMessageContent);
+                chatboxContent.appendChild(userMessageContainer);
+
+                // Find the best response based on patterns
+                let botResponse = "I'm sorry, I don't understand your message.";
+                for (const intent of intents) {
+                    for (const pattern of intent.patterns) {
+                        const regex = new RegExp(pattern, "i"); // Case-insensitive match
+                        if (userMessage.match(regex)) {
+                            botResponse = intent.responses[Math.floor(Math.random() * intent.responses.length)];
+                            break; // Stop searching for patterns once a match is found
                         }
                     }
-
-                    // Create and append the bot's response
-                    const botMessageContainer = document.createElement("div");
-                    botMessageContainer.classList.add("bot-message-container");
-
-                    const botMessageContent = document.createElement("div");
-                    botMessageContent.classList.add("bot-message-content");
-
-                    const botMessage = document.createElement("p");
-                    botMessage.classList.add("bot-message");
-                    botMessage.textContent = "...";
-                    setTimeout(function () {
-                        botMessage.textContent = botResponse;
-                    }, 1000);
-
-                    botMessageContent.appendChild(botMessage);
-                    botMessageContainer.appendChild(botMessageContent);
-                    chatboxContent.appendChild(botMessageContainer);
-
-                    // Clear the input field
-                    messageInput.value = "";
                 }
 
-                sendButton.addEventListener("click", function () {
+                // Create and append the bot's response
+                const botMessageContainer = document.createElement("div");
+                botMessageContainer.classList.add("bot-message-container");
+
+                const botMessageContent = document.createElement("div");
+                botMessageContent.classList.add("bot-message-content");
+
+                const botMessage = document.createElement("p");
+                botMessage.classList.add("bot-message");
+                botMessage.textContent = "...";
+                setTimeout(function () {
+                    botMessage.textContent = botResponse;
+                }, 600);   // make a delay reponse of 600ms
+
+                botMessageContent.appendChild(botMessage);
+                botMessageContainer.appendChild(botMessageContent);
+                chatboxContent.appendChild(botMessageContainer);
+
+                // Clear the input field
+                messageInput.value = "";
+            }
+            //Keep the chatbox container scrolled to the bottom so that new messages are always visible 
+            function scrollToBottom() {
+                chatboxContainer.scrollTop = chatboxContainer.scrollHeight;
+            }
+
+            sendButton.addEventListener("click", function () {
+                const userMessage = messageInput.value;
+                botReply(userMessage);
+                scrollToBottom();
+            });
+
+            messageInput.addEventListener("keyup", function (event) {
+                if (event.key === "Enter") {
                     const userMessage = messageInput.value;
                     botReply(userMessage);
-                });
-
-                messageInput.addEventListener("keyup", function (event) {
-                    if (event.key === "Enter") {
-                        const userMessage = messageInput.value;
-                        botReply(userMessage);
-                    }
-                });
-            })
-            .catch(error => {
-                console.error("Error loading intents:", error);
+                    scrollToBottom();
+                }
             });
-    });
+        })
+        .catch(error => {
+            console.error("Error loading intents:", error);
+        });
+});
